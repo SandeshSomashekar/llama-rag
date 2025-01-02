33971be2631ee60b7cdf25ec5da73e03a8630d54
Sandesh Somashekar <sandesh.somashekar@gmail.com>
2025-01-01 17:57:53 -0700

    Update index.mjs

--- llama-rag/index.mjs
+++ llama-rag/index.mjs
@@ -2,17 +2,12 @@ import express from 'npm:express@4.21.2';
 import basicRouter from './routers/basicRouter.mjs'
 import bodyParser from 'npm:body-parser@1.20.3';
 import cors from 'npm:cors@2.8.5';
-// import express from 'express'
 const port = process.env.PORT || 4001;
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/', basicRouter);
-
-// app.get("/", (req, res) => {
-//     res.send("Welcome to the Dinosaur API!");
-//   });
 
 app.listen(port, () => {
     console.log('Listening on 4001')
